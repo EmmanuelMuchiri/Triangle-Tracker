28dc04c5337ab5eb10c32c780db2c0907be111d4
Emmanuel Muchiri <Emmanuel.Muchiri@outlook.com>
2019-05-30 00:25:18 +0300

    added the condition to check whether the triangle is scalene in nature and also indented my javascript

--- js/script.js
+++ js/script.js
@@ -1,20 +1,13 @@
-var triangleTracker=function(length,width,breadth)
-{
-if (length<=0 || breadth<=0 || width<=0)
-{
-console.log("The numbers you entered are either less than or Equals to Zero or are not Numbers")
-}
-else if
-(length===width && length===breadth && breadth===width)
-{
-console.log("This is an Equilateral Triangle Since All The Dimensions Are Equal");
-}
-else if (length===width || length===breadth || breadth===width)
-{
-console.log("This is an Isosceles Triangle Since Two Dimensions Are Equal");
-}
-else
-{
-console.log("Try again");
-}
+var triangleTracker = function(length, width, breadth) {
+  if (length <= 0 || breadth <= 0 || width <= 0) {
+    console.log("The numbers you entered are either less than or Equals to Zero or are not Numbers")
+  } else if (length === width && length === breadth && breadth === width) {
+    console.log("This is an Equilateral Triangle Since All The Dimensions Are Equal");
+  } else if (length === width || length === breadth || breadth === width) {
+    console.log("This is an Isosceles Triangle Since Two Dimensions Are Equal");
+  } else if (length + width > breadth || (length + breadth > width) || (width + breadth > length)) {
+    console.log("This is A Scalene Triangle Since All Dimensions Are Not Equal");
+  } else {
+    console.log("Try again");
+  }
 }
