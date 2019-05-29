6c212e095489ffcda3c54de96cbe315eb334c825
Emmanuel Muchiri <Emmanuel.Muchiri@outlook.com>
2019-05-30 01:01:51 +0300

    modified my javascript to prompt the user on what to enter

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 var triangleTracker = function(length, width, breadth) {
   if (length <= 0 || breadth <= 0 || width <= 0) {
-    console.log("The numbers you entered are either less than or Equals to Zero or are not Numbers")
+    alert("The numbers you entered are either less than or Equals to Zero or are not Numbers")
   } else if (length === width && length === breadth && breadth === width) {
-    console.log("This is an Equilateral Triangle Since All The Dimensions Are Equal");
+    alert("This is an Equilateral Triangle Since All The Dimensions Are Equal");
   } else if (length === width || length === breadth || breadth === width) {
-    console.log("This is an Isosceles Triangle Since Two Dimensions Are Equal");
+    alert("This is an Isosceles Triangle Since Two Dimensions Are Equal");
   } else if (length + width > breadth || (length + breadth > width) || (width + breadth > length)) {
-    console.log("This is A Scalene Triangle Since All Dimensions Are Not Equal");
+    alert("This is A Scalene Triangle Since All Dimensions Are Not Equal");
   } else {
-    console.log("Try again");
+    alert("Try again");
   }
+  return answer;
 }
-var userLength=prompt("kindly enter the Length of the triangle to determine the type of triangle");
-var userBreadth=prompt("kindly enter the Length of the triangle to determine the type of triangle");
-var userHeight=prompt("kindly enter the Length of the triangle to determine the type of triangle");
+var userLength = prompt("kindly enter the Length of the triangle to determine the type of triangle");
+var userBreadth = prompt("kindly enter the Length of the triangle to determine the type of triangle");
+var userHeight = prompt("kindly enter the Length of the triangle to determine the type of triangle");
 
-alert(triangleTracker(userLength,userBreadth,userHeight));
+alert(triangleTracker(userLength, userBreadth, userHeight));
