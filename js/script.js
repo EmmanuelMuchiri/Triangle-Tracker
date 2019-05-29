989de0110b9e5dced1a0531f91ae6072d15baacf
Emmanuel Muchiri <Emmanuel.Muchiri@outlook.com>
2019-05-30 00:12:19 +0300

    Added the first condition to check whether a number is zero,less than zero or any other data type except number

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ if (length<=0 || breadth<=0 || width<=0)
 {
 console.log("The numbers you entered are either less than or Equals to Zero or are not Numbers")
 }
+else if
+(length===width && length===breadth && breadth===width)
+{
+console.log("This is an Equilateral Triangle because the length Are Equal");
+}
 else
 {
 console.log("Try again");
