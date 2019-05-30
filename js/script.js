d64ad901a0d7ff5f1cec4c5d47b0f083e2b05b1f
Emmanuel Muchiri <Emmanuel.Muchiri@outlook.com>
2019-05-30 13:51:29 +0300

    Added functionality to bothe the submit and reset button then linked it to my javascript file

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
 var triangleTracker = function(length, width, breadth) {
+
+  length = document.dimensions.userLength.value;
+  breadth = document.dimensions.userBreadth.value;
+  width = document.dimensions.userHeight.value;
+
+
   if (length <= 0 || breadth <= 0 || width <= 0) {
     alert("The numbers you entered are either less than or Equals to Zero or are not Numbers")
   } else if (length === width && length === breadth && breadth === width) {
     alert("This is an Equilateral Triangle Since All The Dimensions Are Equal");
-  } else if (length === width || length === breadth || breadth === width) {
+  } else if ((length === width )|| (length === breadth) || (breadth === width)) {
     alert("This is an Isosceles Triangle Since Two Dimensions Are Equal");
   } else if (length + width > breadth || (length + breadth > width) || (width + breadth > length)) {
     alert("This is A Scalene Triangle Since All Dimensions Are Not Equal");
-  } else {
-    alert("Try again");
+  } else if ((length + width) <= breadth || (length + breadth) <= width || (width + breadth) <= length) {
+    alert("This is not a triangle");
+  }
+  else {
+    alert("This is a Scalene Triangle");
   }
   return answer;
 }
-var userLength = prompt("kindly enter the Length of the triangle to determine the type of triangle");
-var userBreadth = prompt("kindly enter the Length of the triangle to determine the type of triangle");
-var userHeight = prompt("kindly enter the Length of the triangle to determine the type of triangle");
-
-alert(triangleTracker(userLength, userBreadth, userHeight));
+function myFunction() {
+  document.getElementById("formElements").reset();
+}
+/*alert(triangleTracker(userLength, userBreadth, userHeight));*/
