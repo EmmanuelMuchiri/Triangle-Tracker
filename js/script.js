ee1618b4eff6b755baad2ae57957aa25b828516a
Emmanuel Muchiri <Emmanuel.Muchiri@outlook.com>
2019-05-31 18:58:15 +0300

    added a few changes

--- js/script.js
+++ js/script.js
@@ -1,27 +1,34 @@
-var triangleTracker = function(length, width, breadth) {
+var triangleTracker = function(length, width, breadth)
+{
 
-  length = document.dimensions.userLength.value;
-  breadth = document.dimensions.userBreadth.value;
-  width = document.dimensions.userHeight.value;
+  length = document.getElementById("userLength").value;
+  breadth = document.getElementById("userBreadth").value;
+  width = document.getElementById("userHeight").value;
 
 
-  if (length <= 0 || breadth <= 0 || width <= 0) {
+  if (length <= 0 || breadth <= 0 || width <= 0)
+  {
     alert("The numbers you entered are either less than or Equals to Zero or are not Numbers")
-  } else if (length === width && length === breadth && breadth === width) {
+  } else if (length === width && length === breadth && breadth === width)
+  {
     alert("This is an Equilateral Triangle Since All The Dimensions Are Equal");
-  } else if ((length === width )|| (length === breadth) || (breadth === width)) {
-    alert("This is an Isosceles Triangle Since Two Dimensions Are Equal");
-  } else if (length + width > breadth || (length + breadth > width) || (width + breadth > length)) {
-    alert("This is A Scalene Triangle Since All Dimensions Are Not Equal");
-  } else if ((length + width) <= breadth || (length + breadth) <= width || (width + breadth) <= length) {
+  }
+  else if ((length + width) <= breadth || (length + breadth) <= width || (width + breadth) <= length)
+  {
     alert("This is not a triangle");
   }
-  else {
+  else if ((length === width )|| (length === breadth) || (breadth === width))
+  {
+    alert("This is an Isosceles Triangle Since Two Dimensions Are Equal");
+  }
+  else
+  {
     alert("This is a Scalene Triangle");
   }
   return answer;
 }
-function myFunction() {
+
+var refresh=function ()
+{
   document.getElementById("formElements").reset();
 }
-/*alert(triangleTracker(userLength, userBreadth, userHeight));*/
